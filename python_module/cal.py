6f9e270f410a361e00c0f51b0993fc7a8c026c42
KyoungYeon <mango037@naver.com>
2019-04-02 14:59:32 +0900

    Update cal.py

--- python_module/cal.py
+++ python_module/cal.py
@@ -9,17 +9,23 @@ while True:
         num1 = int(input("num1: "))
         num2 = int(input("num2: "))
         print(add(num1, num2))
-    if a == '2':
+    elif a == '2':
         num1 = int(input("num1: "))
         num2 = int(input("num2: "))
         print(sub(num1, num2))
-    if a == '3':
+    elif a == '3':
         num1 = int(input("num1: "))
         num2 = int(input("num2: "))
         print(mul(num1, num2))
-    if a == '4':
+    elif a == '4':
         num1 = int(input("num1: "))
         num2 = int(input("num2: "))
-        print(div(num1, num2))
-    if a == '5':
+        if num2 == 0:
+            print("0으로는 나눌 수 없습니다")
+        else:
+            print(div(num1, num2))
+    elif a == '5':
+        print("프로그램을 끝냅니다")
         break
+    else:
+        print("숫자를 제대로 입력하세요")
