84d4912d76da068f5339ab9e91622cbfec6f6212
KyoungYeon <mango037@naver.com>
2019-05-17 19:37:22 +0900

    crawling
    crawling

---
+++ 05.16.crawling/selenium/naver_selenium.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+
+
+print("네이버 실시간 검색순위")
+driver = webdriver.Chrome('./chromedriver')
+
+driver.get("https://www.naver.com")
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+ranks = soup.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k')
+
+texts = []
+count=0
+for title in ranks:
+    count += 1
+    print(str(count) + '. ' + title.text.strip())
+
+driver.quit()
