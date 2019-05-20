b53e9e22c018a79f1f69054102e3f3336920549f
KyoungYeon <mango037@naver.com>
2019-05-20 12:46:13 +0900

    Update naver_selenium.py

--- 05.16.crawling/selenium/naver_selenium.py
+++ 05.16.crawling/selenium/naver_selenium.py
@@ -11,10 +11,9 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 ranks = soup.select('#PM_ID_ct > div.header > div.section_navbar > div.area_hotkeyword.PM_CL_realtimeKeyword_base > div.ah_roll.PM_CL_realtimeKeyword_rolling_base > div > ul > li > a > span.ah_k')
 
-texts = []
 count=0
 for title in ranks:
     count += 1
-    print(str(count) + '. ' + title.text.strip())
+    print(str(count) + '. ' + title.text)
 
 driver.quit()
